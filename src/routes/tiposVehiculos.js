232b64ca6aba8f10a61ecf77619a045b5c865968
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2021-07-14 21:05:31 -0400

    Eliminado controllers

--- src/routes/tiposVehiculos.js
+++ src/routes/tiposVehiculos.js
@@ -9,10 +9,37 @@ const {
 
 const router = new Router();
 
-router.get("/", getTiposVehiculos);
-router.get("/:id", getTipoVehiculo);
-router.post("/", addTipoVehiculo);
-router.put("/:id", updateTipoVehiculo);
-router.delete("/:id", deleteTipoVehiculo);
+// Obtener todos
+router.get("/", async (req, res) => {
+  const tiposVehiculos = await TipoVehiculo.findAll();
+  res.json(tiposVehiculos);
+});
+
+// Obtener uno
+router.get("/:id", async (req, res) => {
+  const codTipoVehiculo = parseInt(req.params.id);
+  const tipoVehiculo = await TipoVehiculo.findById(codTipoVehiculo);
+  res.json(tipoVehiculo);
+});
+
+// Crear uno
+router.post("/", async (req, res) => {
+  const tipoVehiculo = await TipoVehiculo.create(req.body);
+  res.status(201).json(tipoVehiculo);
+});
+
+// Actualizar
+router.put("/:id", async (req, res) => {
+  const codTipoVehiculo = parseInt(req.params.id);
+  const tipoVehiculo = await TipoVehiculo.update(codTipoVehiculo, req.body);
+  res.json(tipoVehiculo);
+});
+
+// Borrar
+router.delete("/:id", async (req, res) => {
+  const codTipoVehiculo = parseInt(req.params.id);
+  await TipoVehiculo.delete(codTipoVehiculo);
+  res.json({ message: "Tipo de Vehiculo eliminado" });
+});
 
 module.exports = router;
