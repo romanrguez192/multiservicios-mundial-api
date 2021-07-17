f970786dca4131165220840147ba410af51efde7
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-07-17 12:12:43 -0400

    Lineas
    Ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const tiposVehiculos = require("./tiposVehiculos");
 const auth = require("./auth");
+const lineas = require("./lineas");
 
 module.exports = (app) => {
   app.use("/api/tiposVehiculos", tiposVehiculos);
   app.use("/api/auth", auth);
+  app.use("/api/lineas", lineas);
 };
