6908218b4781f3a52669b0052fb92a815db347c1
Moniquita <monacuaulma@gmail.com>
2021-07-17 20:41:51 -0400

    Cambios ruta sucursales

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const tiposVehiculos = require("./tiposVehiculos");
+const sucursales = require("./sucursales");
 const auth = require("./auth");
 
 module.exports = (app) => {
   app.use("/api/tiposVehiculos", tiposVehiculos);
+  app.use("/api/sucursales", sucursales);
   app.use("/api/auth", auth);
 };
