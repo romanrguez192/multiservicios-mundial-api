9ffcd9a2f6bb004c2c38f71f2215a380d50a62b4
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-07-18 14:44:40 -0400

    Proveedores
    Listín al parecer

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const lineas = require("./lineas");
 const productosServicios = require("./productosServicios");
 const productosVentas = require("./productosVentas");
 const inventario = require("./inventario");
+const proveedores = require("./proveedores");
 
 module.exports = (app) => {
   app.use("/api/tiposVehiculos", tiposVehiculos);
@@ -12,4 +13,5 @@ module.exports = (app) => {
   app.use("/api/productosServicios", productosServicios);
   app.use("/api/productosVentas", productosVentas);
   app.use("/api/inventario", inventario);
+  app.use("/api/proveedores", proveedores);
 };
