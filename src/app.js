4cf91e3f23748a05aea582de22f86f757802672b
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2021-07-19 22:10:17 -0400

    Revert "passportConfig"
    This reverts commit f5c579bd4baa485f65fd11046d3bc1595023d8cc.

--- src/app.js
+++ src/app.js
@@ -2,28 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const mountRoutes = require("./routes");
 const errorHandler = require("./errorHandler");
-const session = require("express-session");
-const passport = require("passport");
-const initializePassport = require("./passportConfig");
 
 const app = express();
-initializePassport(passport);
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended:false }))
-app.use(session({
-  secret: process.env.SECRET || 'secreto',
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
 
 // Settings
 app.set("port", process.env.PORT || 4000);
-
 
 // Routes
 mountRoutes(app);
