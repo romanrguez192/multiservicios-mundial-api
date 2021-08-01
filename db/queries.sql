40b82b2a7e958665a2b121850d19b97734beb55b
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-08-01 14:12:42 -0400

    Queries estadísticas
    Las 4 primeras, todavía no en los modelos

--- db/queries.sql
+++ db/queries.sql
@@ -50,7 +50,7 @@ WHERE c."cedCliente" = r."cedCliente"
 OR c."cedCliente" = s."cedCliente"
 OR c."cedCliente" = f."cedCliente";
 
-// TODO: OJO si creo una vista para las SS quizás se simplifique
+-- TODO: OJO si creo una vista para las SS quizás se simplifique
 CREATE VIEW "ClientesSucursales" AS
 SELECT "rifSucursal", c."cedCliente", "nombre", "email", "tlfPrincipal", "tlfAlternativo"
 FROM "Clientes" AS c
@@ -68,3 +68,53 @@ SELECT "rifSucursal", c."cedCliente", "nombre", "email", "tlfPrincipal", "tlfAlt
 FROM "Clientes" AS c
 JOIN "FacturasClientes" AS f
 ON c."cedCliente" = f."cedCliente";
+
+-- QUERIES DE ESTADÍSTICAS
+
+-- Clientes frecuentes
+CREATE VIEW "ClientesFrecuentes" AS
+SELECT c."rifSucursal", c."cedCliente", c."nombre", COUNT(*) / 4 AS "promedio",
+CASE
+	WHEN COUNT(*) / 4 BETWEEN 1 AND 2 THEN 5
+	WHEN COUNT(*) / 4 BETWEEN 3 AND 5 THEN 10
+	WHEN COUNT(*) / 4 >= 6 THEN 15
+END AS "descuento"
+FROM "ClientesSucursales" AS c
+JOIN "Vehiculos" AS v
+ON c."cedCliente" = v."cedCliente"
+JOIN "SolicitudesServicio" AS s
+ON s."codVehiculo" = v."codVehiculo"
+WHERE s."rifSucursal" = c."rifSucursal"
+AND NOW() - s."fechaEntrada" < '4 months'
+GROUP BY c."rifSucursal", c."cedCliente", c."nombre"
+HAVING COUNT(*) / 4 >= 1;
+
+-- Clientes que no usan los servicios tras reservar
+SELECT DISTINCT c."cedCliente", c."nombre", COUNT(*) AS "totalVeces"
+FROM "Clientes" AS c
+JOIN "Reservaciones" AS r
+ON c."cedCliente" = r."cedCliente"
+WHERE r."rifSucursal" = $1
+AND r."status" = 'perdida'
+GROUP BY c."cedCliente", c."nombre";
+
+-- Marcas más atendidas por servicio
+SELECT s."codServicio", s."nombre", v."marca", COUNT(DISTINCT ss."nroSolicitud") AS "totalVeces"
+FROM "Servicios" AS s
+JOIN "DetallesSolicitudes" AS ds
+ON s."codServicio" = ds."codServicio"
+JOIN "SolicitudesServicio" AS ss
+ON ss."nroSolicitud" = ds."nroSolicitud"
+JOIN "Vehiculos" AS v
+ON ss."codVehiculo" = v."codVehiculo"
+WHERE ss."rifSucursal" = $1
+GROUP BY s."codServicio", s."nombre", v."marca";
+
+-- Personal que realiza más servicios por mes
+-- TODO: El mes
+SELECT e."cedEmpleado", CONCAT(e."nombre", ' ', e."apellido") AS "nombreEmpleado", COUNT(DISTINCT os."nroSolicitud") AS "totalServicios"
+FROM "Empleados" AS e
+JOIN "OrdenesServicio" AS os
+ON e."cedEmpleado" = os."cedEmpleado"
+WHERE e."rifSucursal" = '799072750'
+GROUP BY e."cedEmpleado", e."nombre";
