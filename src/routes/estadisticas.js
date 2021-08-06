89887fe133a70347c7e1f7922e4a97ac4f32c79b
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2021-08-06 00:25:41 -0400

    agregadas rutas a estadisticas

--- src/routes/estadisticas.js
+++ src/routes/estadisticas.js
@@ -1,5 +1,7 @@
 const Router = require("express-promise-router");
 const EstadisticasClientes = require("../models/EstadisticasClientes");
+const EstadisticasMarcas = require("../models/EstadisticasMarcas");
+const EstadisticasPersonal = require("../models/EstadisticasPersonal");
 
 const router = new Router();
 
@@ -13,6 +15,18 @@ router.get("/clientesNoUsanServicio", async(req, res) => {
   const rifSucursal = req.params.id;
   const clientes = await EstadisticasClientes.noUsanServicio(rifSucursal);
   res.json(clientes);
+})
+
+router.get("/marcasMasAtendidas", async(req, res) => {
+  const rifSucursal = req.params.id;
+  const marcas = await EstadisticasMarcas.marcasMasAtendidas(rifSucursal);
+  res.json(marcas);
+})
+
+router.get("/personalMasMenosServicios", async(req, res) => {
+  const rifSucursal = req.params.id;
+  const personal = await EstadisticasPersonal.personalQueRealizaServicios(rifSucursal);
+  res.json(personal);
 })
 
 module.exports = router;
